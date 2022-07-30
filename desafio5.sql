b574f7b224f7ff5d54e51dcf58ce892f0e0503d7
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-07-30 18:51:59 -0400

    fix: corrigi desafio5

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,9 @@
 SELECT 
-    c.cancao AS cancao, COUNT(h.cancao_id) AS reproducoes
+    c.cancao AS cancao, COUNT(c.cancao_id) AS reproducoes
 FROM
     SpotifyClone.cancoes AS c
         INNER JOIN
     SpotifyClone.historico AS h ON c.cancao_id = h.cancao_id
+GROUP BY c.cancao
 ORDER BY reproducoes DESC , cancao ASC
 LIMIT 2;
