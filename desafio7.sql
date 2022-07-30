01cb62f35a10f085e0c4e26c715ff4fef369d87f
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-07-30 19:34:26 -0400

    feat: resolve o desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,12 @@
+SELECT 
+    art.artista AS artista,
+    alb.album AS album,
+    COUNT(seg.usuario_id) AS seguidores
+FROM
+    SpotifyClone.artistas AS art
+        INNER JOIN
+    SpotifyClone.albuns AS alb ON art.artista_id = alb.artista_id
+        INNER JOIN
+    SpotifyClone.seguidores AS seg ON art.artista_id = seg.artista_id
+GROUP BY artista , album
+ORDER BY seguidores DESC , artista , album;
