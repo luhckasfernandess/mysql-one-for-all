e5659f4be1c0ebc291d6ed8b5e91da43ba372a82
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-07-30 16:15:37 -0400

    feat: resolve desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,11 @@
+SELECT 
+    u.usuario AS usuario,
+    IF(YEAR(h.data_reproducao) = 2021,
+        'Usuário ativo',
+        'Usuário inativo') AS condicao_usuario
+FROM
+    SpotifyClone.usuarios AS u
+        INNER JOIN
+    SpotifyClone.historico AS h ON u.usuario_id = h.usuario_id
+GROUP BY u.usuario
+ORDER BY u.usuario;
